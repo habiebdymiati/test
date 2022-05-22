48cba3e8d1253f5147f853052b6f3b0508ac7ebb
habiebdymiati <habiebdymiati@gmail.com>
2022-05-23 06:50:01 +0800

    Belajar Mei

---
+++ kalkulator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ( ) {
+	float a,b,hasil;
+	char arimatika;
+	cin >> a >> arimatika >> b;
+	switch (arimatika) {
+    case '+': hasil = a+b;cout <<hasil<<endl;break;
+    case '-': hasil = a-b;cout <<hasil<<endl;break;
+    case '*': hasil = a*b;cout <<hasil<<endl;break;
+    case '/': hasil = a/b;cout <<hasil<<endl;break;
+    default: cout << "Operator salah" <<endl;}
+	return 0;
+}
