d1544dd5db4684f8d39c83bbd33ceee828dddf7f
habiebdymiati <habiebdymiati@gmail.com>
2022-05-24 06:35:40 +0800

    Latihan

--- segitiga sama sisi.cpp
+++ segitiga sama sisi.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main ( ) {
-    int n;
+    int n, x=0;
     cout << "Masukan angka n" <<" ";
     cin >> n;
     for (int i = 1; i<=n; i++) {
         for (int j=1;j<=i;j++) {
-            cout <<"*";
+                if (x>9){x=0;}
+                    else {x=x;}
+            cout <<x; x++;
         } cout <<endl;
     }
 }
